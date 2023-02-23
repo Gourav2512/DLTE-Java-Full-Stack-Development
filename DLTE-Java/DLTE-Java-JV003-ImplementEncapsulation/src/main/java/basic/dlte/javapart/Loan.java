4a17718c7af8ca04e9baaf77de2dc966db1e4b7f
Gourav2512 <gouravshetty2512@gmail.com>
2023-02-23 23:27:19 +0530

    Encapsulation example

---
+++ DLTE-Java/DLTE-Java-JV003-ImplementEncapsulation/src/main/java/basic/dlte/javapart/Loan.java
@@ -0,0 +1,79 @@
+package basic.dlte.javapart;
+
+import java.util.Scanner;
+
+public class Loan {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Customer customer = new Customer();
+        System.out.println("Enter your name:");
+        customer.setName(sc.nextLine());
+        System.out.println("Enter PAN Number:");
+        customer.setPan(sc.nextLine());
+        System.out.println("Enter Aadhaar Number:");
+        customer.setAadhaar(sc.nextLine());
+        System.out.println("Enter your profession:");
+        customer.setProfession(sc.nextLine());
+        System.out.println("Enter your CIBIL score:");
+        customer.setCibil(sc.nextInt());
+        sc.nextLine();
+        System.out.println("Enter Contact Number:");
+        customer.setContact(sc.nextLine());
+
+        System.out.println("\nYour loan request has been received with following details,");
+        System.out.println("Name: "+customer.getName()+"\nPAN: "+customer.getPan()+"\nAadhaar Number: "+customer.getAadhaar());
+        System.out.println("Profession: "+customer.getProfession()+"\nCIBIL Score: "+customer.getCibil()+"\nContact Number: "+customer.getContact());
+
+
+
+    }
+}
+class Customer{
+    private String name;
+    private String pan;
+    private String aadhaar;
+    private String profession;
+    private Integer cibil;
+    private String contact;
+
+    //Getter methods
+    String getName(){
+        return this.name;
+    }
+    String getPan(){
+        return this.pan;
+    }
+    String getAadhaar(){
+        return this.aadhaar;
+    }
+    String getProfession(){
+        return this.profession;
+    }
+    Integer getCibil(){
+        return this.cibil;
+    }
+    String getContact(){
+        return this.contact;
+    }
+
+    //Setter Methods
+    void setName(String name){
+        this.name = name;
+    }
+    void setPan(String pan){
+        this.pan = pan;
+    }
+    void setAadhaar(String aadhaar){
+        this.aadhaar = aadhaar;
+    }
+    void setProfession(String profession){
+        this.profession = profession;
+    }
+    void  setCibil(int cibil){
+        this.cibil = cibil;
+    }
+    void setContact(String contact){
+        this.contact = contact;
+    }
+
+}
