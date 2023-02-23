439874056f132379ed8fedb083a1b5a88bb1b415
Gourav2512 <gouravshetty2512@gmail.com>
2023-02-24 04:06:40 +0530

    Gpay(final)

--- DLTE-Java/DLTE-Java-JV004-ConsoleGPay/src/main/java/org/example/Application.java
+++ DLTE-Java/DLTE-Java-JV004-ConsoleGPay/src/main/java/org/example/Application.java
@@ -1,92 +1,95 @@
 package org.example;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.util.Scanner;
-
-@Data
-
 
 class Gpay{
     Scanner sc = new Scanner(System.in);
 
     private String accno;
-    private  int balance;
-    private int upi;
+    private  Integer balance;
+    private Integer upi;
     private String name;
 
-    public Gpay(String accno, int balance, int upi, String name) {
+    //Constructor
+    public Gpay(String accno, Integer balance, Integer upi, String name) {
         this.accno = accno;
         this.balance = balance;
         this.upi = upi;
         this.name = name;
     }
-
-    public String getAccno() {
-        return accno;
-    }
-
-    public void setAccno(String accno) {
-        this.accno = accno;
-    }
-
-    public int getBalance() {
-        return balance;
-    }
-
-    public void setBalance(int balance) {
-        this.balance = balance;
-    }
-
-    public int getUpi() {
-        return upi;
-    }
-
-    public void setUpi(int upi) {
-        this.upi = upi;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "Gpay{" +
-                "accno=" + accno +
-                ", balance=" + balance +
-                ", upi=" + upi +
-                ", name='" + name + '\'' +
-                '}';
-    }
-    public void fundTransfer(){
-        String recep;
-        Integer amount;
-        System.out.println("Enter the name:");
-        recep = sc.nextLine();
-        System.out.println("Enter the amount:");
-        amount = sc.nextInt();
-        System.out.println("Enter the upi:");
+    //Fund Transfer Method
+    public void fundTransfer(String recep, Integer amount){
+        if(amount>this.balance){
+            System.out.println("Insufficient balance...");
+            return;
+        }
+        System.out.println("Enter the UPI pin: ");
         if(this.upi==sc.nextInt()){
-            System.out.println(amount.toString()+" transferred to "+recep);
+            System.out.println(amount+" transferred to "+recep);
         }
         else {
             System.out.println("Invalid UPI pin");
         }
+    }
+    //Method Overloading
+    public void recharge(Long number, Integer amount){
+        if(amount>this.balance){
+            System.out.println("Insufficient balance...");
+            return;
+        }
+        System.out.println("Enter the UPI pin: ");
+        if(this.upi==sc.nextInt()){
+            System.out.println(number+" successfully recharged with "+amount);
+        }
+        else {
+            System.out.println("Invalid UPI pin");
+        }
+    }
 
-
+    public void recharge(String vehicle, Integer amount){
+        if(amount>this.balance){
+            System.out.println("Insufficient balance...");
+            return;
+        }
+        System.out.println("Enter the UPI pin: ");
+        if(this.upi==sc.nextInt()){
+            System.out.println("FASTag for "+vehicle+" successfully recharged with "+amount);
+        }
+        else {
+            System.out.println("Invalid UPI pin");
+        }
     }
 }
+
 public class Application {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Integer amount;
         Gpay user = new Gpay("123456789",5000,4321,"Gourav");
-        user.fundTransfer();
+        System.out.println("1. Fund Transfer\n2. Mobile Recharge\n3. FASTag Recharge\nEnter your choice:");
+        switch (sc.nextInt()){
+            case 1: sc.nextLine();
+                    System.out.println("Enter the name:");
+                    String recep = sc.nextLine();
+                    System.out.println("Enter the amount:");
+                    amount = sc.nextInt();
+                    user.fundTransfer(recep,amount);
+                    break;
+            case 2: System.out.println("Enter the mobile number:");
+                    Long number = sc.nextLong();
+                    System.out.println("Enter the amount:");
+                    amount = sc.nextInt();
+                    user.recharge(number,amount);
+                    break;
+            case 3: System.out.println("Enter the vehicle number:");
+                    sc.nextLine();
+                    String vehicle = sc.nextLine();
+                    System.out.println("Enter the amount:");
+                    amount = sc.nextInt();
+                    user.recharge(vehicle,amount);
+                    break;
+            default: System.out.println("Invalid Choice");
+        }
     }
 }
 
